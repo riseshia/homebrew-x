09332be7746f2cea9afab3466512fa57acb4cb45
Shia <rise.shia@gmail.com>
2024-06-13 23:51:56 +0900

    comment out bottle
    Closes #4.
    
    Signed-off-by: Shia <rise.shia@gmail.com>

--- Formula/envdb.rb
+++ Formula/envdb.rb
@@ -5,11 +5,6 @@ class Envdb < Formula
   sha256 "8c2b33df9f5ccf0b077b796dd1166c9ebfdbde1dcc8ba131d3d900254b258ecb"
   license "MIT"
 
-  bottle do
-    sha256 cellar: :any,                 ventura:        "1242ba844f8dd5fe9d7e1a981a5b2570f135d9c379fdf89b7e7dd5e14a281d7f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "95bd0e0bb8ed5d17049c4deca51d4c33112f2dd6852cdb5df86254823126225c"
-  end
-
   depends_on "rust" => :build
 
   def install
