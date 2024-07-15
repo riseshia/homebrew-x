b4ed6028ff9ed2f28d6125550a69ee18530a3b1e
Shia <rise.shia@gmail.com>
2024-07-15 14:26:52 +0900

    Add fubura to tap

---
+++ Formula/fubura.rb
@@ -0,0 +1,17 @@
+class Fubura < Formula
+  desc "CLI for managing Step Function states with EventBridge Scheduler at once"
+  homepage "https://github.com/riseshia/fubura"
+  url "https://github.com/riseshia/fubura/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "319878a69dba7602fcfb43a57489f62170332a4a836c74cfa9d247776e633537"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/fubura", "help"
+  end
+end
