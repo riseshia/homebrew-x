6750a972ada81f567d763c922c7483260e851109
Shia <rise.shia@gmail.com>
2024-07-25 20:51:36 +0900

    Update fubura to 0.1.1

--- Formula/fubura.rb
+++ Formula/fubura.rb
@@ -1,15 +1,9 @@
 class Fubura < Formula
   desc "CLI for managing Step Function states with EventBridge Scheduler at once"
   homepage "https://github.com/riseshia/fubura"
-  url "https://github.com/riseshia/fubura/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "319878a69dba7602fcfb43a57489f62170332a4a836c74cfa9d247776e633537"
+  url "https://github.com/riseshia/fubura/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "8930c0a91013d10913dba0fdc88bea0a281b462ffd50d0503b9904ff004bd92f"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/riseshia/homebrew-x/releases/download/fubura-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0fa667ac6d03b4acc305ef1a8c21b63fa7ec44f1842af9981772c65fd53d2b30"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "99e75086737c089fb3c45b1bf715989f05a873d9938ee5a7487d4d767403d173"
-  end
 
   depends_on "rust" => :build
 
