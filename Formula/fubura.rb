f8624ed0689a4c18e0eb539b8e566135e8150a7e
Shia <rise.shia@gmail.com>
2024-08-01 11:07:57 +0000

    fubura: add 0.1.3 bottle.

--- Formula/fubura.rb
+++ Formula/fubura.rb
@@ -5,6 +5,12 @@ class Fubura < Formula
   sha256 "e0062f927373b61ab2d167950c3c85b00fc85f20cb7bc8d1821a1e6492d1e175"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/riseshia/homebrew-x/releases/download/fubura-0.1.3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "466d11c5c7e0ed7c2872e5d730dfa7579d9063ead3672bec13d36c166228a517"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a398fc74d2798716e0088da0daef5e9ca3f574fbf39caa9189633b12e5028ade"
+  end
+
   depends_on "rust" => :build
 
   def install
