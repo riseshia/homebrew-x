f437e339463a1b5ae4afdf8f5746e87e669cbb53
Shia <rise.shia@gmail.com>
2024-09-30 23:25:25 +0000

    fubura: update 0.1.7 bottle.

--- Formula/fubura.rb
+++ Formula/fubura.rb
@@ -6,9 +6,9 @@ class Fubura < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/riseshia/homebrew-x/releases/download/fubura-0.1.6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "2393ca45def60500cb7c229494327b475c1fb0c57016fc857ec93889fb865588"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "568a05be6c7ac288fa0bfdfe9aea251ec9d4e0315a6bbe1210110bf7f4c505ed"
+    root_url "https://github.com/riseshia/homebrew-x/releases/download/fubura-0.1.7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1db47f9b950a55e041b21f6905cead4e35d626751c5b3a611d423bbb2ad30161"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ff706d0b6943374a392d94c1f9364f4b13a81847c4acc0592fa197d7c87a042f"
   end
 
   depends_on "rust" => :build
